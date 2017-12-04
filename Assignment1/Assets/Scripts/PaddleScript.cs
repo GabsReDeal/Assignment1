ef67c3b3a5d91811ef9aa79321d6e679c531f120
GabsReDeal <louisgabriel@ymail.com>
2017-12-04 14:06:36 +0100

    - The blue paddle can be moved via up and down arrow keys. - removed the seperate paddle scripts and merged them into one.

---
+++ Assignment1/Assets/Scripts/PaddleScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleScript : MonoBehaviour {
+
+    public float blueSpeed;
+    public Rigidbody2D bluePaddleRb;
+    public GameObject bluePaddle;
+    public GameObject redPaddle;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Red paddle controls
+        float mousePosInUnits = (Input.mousePosition.y / Screen.height * 14) - 8;
+
+        Vector3 newPaddlePos = new Vector3(redPaddle.transform.position.x, mousePosInUnits, redPaddle.transform.position.z);
+        newPaddlePos.y = Mathf.Clamp(mousePosInUnits, -7.5f, 7.5f);
+
+        redPaddle.transform.position = newPaddlePos;
+
+
+        //Blue paddle controls
+        if (Input.GetKey("up"))
+        {
+            //print("up");
+            bluePaddleRb.AddForce(bluePaddle.transform.right * blueSpeed);
+        }
+
+        else if (Input.GetKey("down"))
+        {
+            //print("down");
+            bluePaddleRb.AddForce(bluePaddle.transform.right * (blueSpeed * -1));
+        }
+    }
+}
